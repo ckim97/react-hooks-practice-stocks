a3ea98607b274543dc1122cca54aaa79f6c2e7b2
Christopher <chrisjwkim97@gmail.com>
2023-11-30 21:10:21 -0500

    progress

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+  const [stocks, setStocks] = useState([]);
+  const [boughtStocks, setBoughtStocks] = useState([]);
+  
+  console.log(boughtStocks);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then(r => r.json())
+      .then((data) => setStocks(data))
+  },[]);
+
+  function handleAddStock(stockToAdd) {
+    const stockInPortfolio = boughtStocks.find(
+      (stock) => stock.id === stockToAdd.id
+    );
+    if (!stockInPortfolio) {
+      setBoughtStocks([...boughtStocks, stockToAdd]);
+    }
+  }
+
+function handleRemoveStock(stockObject) {
+  setBoughtStocks((boughtStocks) => 
+    boughtStocks.filter((stock) => stock.id !== stockObject.id)
+  );
+}
+  
+
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} onAddStock={handleAddStock}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer boughtStocks={boughtStocks} onRemoveStock={handleRemoveStock}/>
         </div>
       </div>
     </div>
